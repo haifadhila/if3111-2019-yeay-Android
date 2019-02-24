8c7c6bce38faa07a9486e4b5e2f31242bdac98d3
haifadhila <haifadhila.ilma@gmail.com>
2019-02-24 23:11:11 +0700

    edit tabfragment1

--- app/src/main/java/com/example/catsmanager/TabFragment1.java
+++ app/src/main/java/com/example/catsmanager/TabFragment1.java
@@ -23,10 +23,6 @@ import com.google.android.gms.tasks.OnSuccessListener;
  */
 public class TabFragment1 extends Fragment {
 
-    private FusedLocationProviderClient fusedLocationClient;
-    private Button intentButton;
-    private Location loc;
-
     public TabFragment1() {
         // Required empty public constructor
     }
@@ -35,46 +31,7 @@ public class TabFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.tab_fragment1, container, false);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            loc = location;
-                            Log.d("YAGITU",location.toString());
-                        }
-                    }
-                });
-
-        intentButton = (Button) view.findViewById(R.id.button);
-        intentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMaps(view,loc);
-            }
-        });
-
-        // Inflate the layout for this fragment
         return view;
     }
-
-
-
-    public void openMaps(View v, Location location) {
-        // Create a Uri from an intent string. Use the result to create an Intent.
-        Uri gmmIntentUri = Uri.parse("geo:0,0?q="+location.getLatitude()+","+location.getLongitude()+"(You found the Cat here!)");
-
-        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        // Make the Intent explicit by setting the Google Maps package
-        mapIntent.setPackage("com.google.android.apps.maps");
-
-        // Attempt to start an activity that can handle the Intent
-        startActivity(mapIntent);
-    }
-
-
 
 }
