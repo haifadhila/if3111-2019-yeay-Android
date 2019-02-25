67e5f3845dc6f88e5238855fa0b86b2346b84e4b
Nadija Herdwina <13516130@std.stei.itb.ac.id>
2019-02-25 18:06:01 +0700

    intent on start button

--- app/src/main/java/com/example/catsmanager/TabFragment1.java
+++ app/src/main/java/com/example/catsmanager/TabFragment1.java
@@ -5,6 +5,8 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.net.Uri;
@@ -26,17 +28,19 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import static android.content.Context.SENSOR_SERVICE;
 import static android.support.v4.content.ContextCompat.getSystemService;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TabFragment1 extends Fragment{
+public class TabFragment1 extends Fragment {
+
+    private SensorManager sensorManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         Context context = null;
         View view = inflater.inflate(R.layout.tab_fragment1, container, false);
         final Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
@@ -51,7 +55,6 @@ public class TabFragment1 extends Fragment{
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
                 String text = parent.getItemAtPosition(i).toString();
-                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -61,37 +64,17 @@ public class TabFragment1 extends Fragment{
         });
 
         // handle start button to start countdown
+        sensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        final Sensor proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         Button startButton = (Button) view.findViewById(R.id.startButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                focusStartAlert.setText("Put your phone down!");
                 String timeValue =  spinner.getSelectedItem().toString();
-                Integer focus_time = 0;
-                Log.d("nama", timeValue);
-                if (timeValue.equals("15 minutes")) {
-                    focus_time = 15;
-                } else if (timeValue.equals("30 minutes")) {
-                    focus_time = 30;
-                } else if (timeValue.equals("45 minutes")) {
-                    focus_time = 45;
-                } else if (timeValue.equals("1 hour")) {
-                    focus_time = 60;
-                } else if (timeValue.equals("2 hours")) {
-                    focus_time = 120;
-                }
-
-                //check proximity
                 Intent myIntent = new Intent(getActivity(), ProximityActivity.class);
-                TabFragment1.this.startActivity(myIntent);
-//                Sensor myProximitySensor = mySensorManager.getDefaultSensor(
-//                        Sensor.TYPE_PROXIMITY);
-//                if (myProximitySensor == null){
-//                    focusStartAlert.setText("No Proximity Sensor!");
-//                }else{
-//                    focusStartAlert.setText("Proximity Sensor Available");
-//                }
-                //start countdown
+                myIntent.putExtra("timeValue", timeValue);
+                startActivity(myIntent);
+
             }
         });
         return view;
